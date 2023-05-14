f7d0e15055fee6b13822f2860bd662af5a5dbbab
Romaty03 <ojeniyirukoyah6@gmail.com>
2023-05-14 15:47:15 -0700

    Uploading files

---
+++ database.sql
@@ -0,0 +1,108 @@
+USE [master]
+GO
+
+/****** Object:  Database [StudentRegistration]    Script Date: 2/11/2022 12:10:05 PM ******/
+CREATE DATABASE [StudentRegistration]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'StudentRegistration', FILENAME = N'C:\Users\Ojeniyi Rukoyah\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\StudentRegistration.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'StudentRegistration_log', FILENAME = N'C:\Users\Ojeniyi Rukoyah\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\StudentRegistration.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [StudentRegistration].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [StudentRegistration] SET ANSI_NULL_DEFAULT ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET ANSI_NULLS ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET ANSI_PADDING ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET ANSI_WARNINGS ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET ARITHABORT ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET CURSOR_DEFAULT  LOCAL 
+GO
+
+ALTER DATABASE [StudentRegistration] SET CONCAT_NULL_YIELDS_NULL ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET QUOTED_IDENTIFIER ON 
+GO
+
+ALTER DATABASE [StudentRegistration] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [StudentRegistration] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [StudentRegistration] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [StudentRegistration] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [StudentRegistration] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [StudentRegistration] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [StudentRegistration] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [StudentRegistration] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [StudentRegistration] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [StudentRegistration] SET  READ_WRITE 
+GO
+
